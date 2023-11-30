54c98d05cb8aeff9311d92075f1b0c20305dabf3
Advocateilker <advocate.ilker@gmail.com>
2023-11-30 11:19:32 +0300

    AuthPage Sayfası düzenlendi

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,17 +1,10 @@
 import { signInWithPopup } from 'firebase/auth'
 import React from 'react'
-
 import { auth, provider } from '../firebase/config'
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
-
-
-
-const AuthPage = ({user}) => {
-
-    const navigate=useNavigate()
-
-
+const AuthPage = ({ user }) => {
+    const navigate = useNavigate()
     const handleLogin = () => {
         signInWithPopup(auth, provider)
             .then((res) => {
@@ -23,23 +16,23 @@ const AuthPage = ({user}) => {
 
     }
 
-    if(user){
+    if (user) {
         navigate("/home")
-    }else
-    
-    return (
-        <div className='container'>
-            <div className='auth'>
-                <h1>BİLET EDİN</h1>
-                <p>Devam Etmek İçin Giriş Yapınız</p>
-                <button onClick={handleLogin}>
-                    <img src="./google.png" alt="" />
-                    <span>Sign in with Google</span>
-                </button>
-            </div>
+    } else
 
-        </div>
-    )
+        return (
+            <div className='container'>
+                <div className='auth'>
+                    <h1>BİLET EDİN</h1>
+                    <p>Devam Etmek İçin Giriş Yapınız</p>
+                    <button onClick={handleLogin}>
+                        <img src="./google.png" alt="" />
+                        <span>Sign in with Google</span>
+                    </button>
+                </div>
+
+            </div>
+        )
 }
 
 export default AuthPage
