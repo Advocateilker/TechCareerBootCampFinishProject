d95bdeeb7214a1b59c85a95f82d38758dd8d32de
Advocateilker <advocate.ilker@gmail.com>
2023-11-30 11:30:26 +0300

    PlaceEvents sayfası düzenlendi

--- src/pages/PlaceEvents.jsx
+++ src/pages/PlaceEvents.jsx
@@ -4,12 +4,8 @@ import Card from "../components/Card"
 
 
 const PlaceEvents = ({events}) => {
-
     let { place } = useParams();
-
     const filtered=events?.filter((i)=> i?.place==place)
-
-    console.log(filtered)
   return (
     <div className="card-container">
       {filtered?.map((event)=> <Card event={event}/>)}
