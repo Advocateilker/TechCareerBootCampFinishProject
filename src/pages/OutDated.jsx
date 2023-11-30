3ab6c35530414b5a08c000e70d51d1fb84a4ef3d
Advocateilker <advocate.ilker@gmail.com>
2023-11-30 11:29:55 +0300

    OutDated sayfası düzenlendi

--- src/pages/OutDated.jsx
+++ src/pages/OutDated.jsx
@@ -1,20 +1,13 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import Card from '../components/Card'
 import OutdatedTable from '../components/OutdatedTable'
-
 const OutDated = () => {
-
     const [outdated, setOutdated] = useState()
-
     useEffect(() => {
         axios.get("http://localhost:3038/outdated")
             .then((res) => setOutdated(res.data))
             .catch((err) => console.log(err))
     }, [])
-
-   // console.log(outdated)
-
     return (
         <div >
             <h1 className='text-center text-danger'>Süresi Geçmiş Etkinlikler</h1>
