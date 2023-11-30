f9f652a9a5fd96e29d4d08076a7b55851fcd4fbe
Advocateilker <advocate.ilker@gmail.com>
2023-11-30 11:18:42 +0300

    Sidebar Componenti düzenlendi

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-
 const Sidebar = ({categories,filterCategory}) => {
-
-
-
   return (
     <div className='sidebar'>
       <h5 className='text-decoration-underline'>Kategoriler</h5>
