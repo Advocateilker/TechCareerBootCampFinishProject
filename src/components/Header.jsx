7f4ebfdb7ea63aa7e860c7aaaabd284f7c426520
Advocateilker <advocate.ilker@gmail.com>
2023-12-04 16:08:18 +0300

    header kısmında ve tasarımında düzenlemeler yapıldı

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import {useState} from 'react'
 import { NavLink, useNavigate } from "react-router-dom";
+
 const Header = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate()
+  const [hidden,setHidden]=useState(false)
   return (
     <header>
-      <div onClick={()=>navigate("/home")} className='left'>BiletEdin</div>
-      <div className='right'>
+      <div onClick={() => navigate("/home")} className='left'>BiletEdin</div>
+      <div className='right' >
         <NavLink to={"/feed"}>Etkinlikleri Liste</NavLink>
         <NavLink to={"/map"}>Harita Görünümü</NavLink>
         <NavLink to={"/outdated"}>Zamanı Geçmiş Etkinlikler</NavLink>
       </div>
+
     </header>
   )
 }
