6311dce383eb2c72e5520f143376b15c5767bda7
Advocateilker <advocate.ilker@gmail.com>
2023-12-07 12:18:14 +0300

    son düzenlemeler tamamlandı

---
+++ src/components/NoAuth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoAuth = () => {
+  return (
+    <div>
+        <img src="https://media.tenor.com/imQwGvH_Zr0AAAAM/wag-finger-nope.gif" alt="" />
+    </div>
+  )
+}
+
+export default NoAuth
